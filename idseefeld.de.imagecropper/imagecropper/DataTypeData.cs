cdf9635082d2723011e5b04b792fb4f250935270
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2013-09-10 14:55:49 +0200

    fixes #9 - broken property xml value

--- idseefeld.de.imagecropper/imagecropper/DataTypeData.cs
+++ idseefeld.de.imagecropper/imagecropper/DataTypeData.cs
@@ -2,20 +2,23 @@
 
 namespace idseefeld.de.imagecropper.imagecropper
 {
-    public class DataTypeData : umbraco.cms.businesslogic.datatype.DefaultData
+	public class DataTypeData : umbraco.cms.businesslogic.datatype.FileHandlerData
     {
-        public DataTypeData(umbraco.cms.businesslogic.datatype.BaseDataType DataType) : base(DataType) { }
+        public DataTypeData(umbraco.cms.businesslogic.datatype.BaseDataType DataType) : base(DataType, "") { }
 
-        public override XmlNode ToXMl(XmlDocument data)
-        {
-            if (Value.ToString() != "") {
-                XmlDocument xd = new XmlDocument();
-                xd.LoadXml(Value.ToString());
-                return data.ImportNode(xd.DocumentElement, true);
-            } else {
-                return base.ToXMl(data);
-            }
+		public override XmlNode ToXMl(XmlDocument data)
+		{
+			if (Value.ToString() != "")
+			{
+				XmlDocument xd = new XmlDocument();
+				xd.LoadXml(Value.ToString());
+				return data.ImportNode(xd.DocumentElement, true);
+			}
+			else
+			{
+				return base.ToXMl(data);
+			}
 
-        }
+		}
     }
 }
